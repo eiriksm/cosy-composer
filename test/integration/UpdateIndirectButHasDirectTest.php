3bd362e4c15386ea73baaeb8780f1344fc0dac94
Eirik S. Morland <eirik@morland.no>
2022-03-03 21:20:30 +0100

    Add better check for filterer output

--- test/integration/UpdateIndirectButHasDirectTest.php
+++ test/integration/UpdateIndirectButHasDirectTest.php
@@ -15,6 +15,23 @@ class UpdateIndirectButHasDirectTest extends ComposerUpdateIntegrationBase
     {
         $this->runtestExpectedOutput();
         self::assertEquals('Update symfony/polyfill-mbstring from v1.23.0 to v1.24.0', $this->prParams["title"]);
-        self::assertEquals('symfonypolyfillmbstringv1230v1240', $this->prParams["head"]);
+        $output = $this->cosy->getOutput();
+        $found_msg = false;
+        foreach ($output as $item) {
+            if ($item->getType() !== 'update') {
+                continue;
+            }
+            $context = $item->getContext();
+            foreach ($context['packages'] as $package) {
+                if (empty($package->name) || $package->name !== 'symfony/polyfill-mbstring') {
+                    continue;
+                }
+                if (empty($package->latest) || $package->latest !== 'v1.24.0') {
+                    continue;
+                }
+                $found_msg = true;
+            }
+        }
+        self::assertEquals(true, $found_msg);
     }
 }

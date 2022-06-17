f73fcd865877bd3367f964997ee7816a22633f86
Eirik S. Morland <eirik@morland.no>
2022-06-17 08:07:25 +0200

    Add basic automerge test

---
+++ test/integration/AutomergeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+/**
+ * Test for automerge being enabled.
+ */
+class AutomergeTest extends ComposerUpdateIntegrationBase
+{
+    protected $composerAssetFiles = 'composer.automerge';
+    protected $hasUpdatedPsrLog = false;
+    protected $hasUpdatedPsrCache = false;
+    protected $packageForUpdateOutput = 'psr/log';
+    protected $packageVersionForFromUpdateOutput = '1.1.3';
+    protected $packageVersionForToUpdateOutput = '1.1.4';
+    protected $hasAutoMerge = true;
+    protected $checkPrUrl = true;
+
+    public function testAutomerge()
+    {
+        $this->runtestExpectedOutput();
+    }
+
+}

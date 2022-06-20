41e082470dc494ca72cad86a70ef3eb4d2da099f
Eirik S. Morland <eirik@morland.no>
2022-06-20 07:20:14 +0200

    Add a test for automerge security with no security update

---
+++ test/integration/AutomergeSecurityNoUpdateTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+/**
+ * Test for automerge being enabled for security, but no security updates.
+ */
+class AutomergeSecurityNoUpdateTest extends ComposerUpdateIntegrationBase
+{
+    protected $composerAssetFiles = 'composer.automerge_sec_no_update';
+    protected $hasUpdatedPsrLog = false;
+    protected $hasUpdatedPsrCache = false;
+    protected $packageForUpdateOutput = 'psr/log';
+    protected $packageVersionForFromUpdateOutput = '1.1.3';
+    protected $packageVersionForToUpdateOutput = '1.1.4';
+    protected $hasAutoMerge = false;
+    protected $checkPrUrl = true;
+
+    public function testAutomerge()
+    {
+        $this->runtestExpectedOutput();
+    }
+}

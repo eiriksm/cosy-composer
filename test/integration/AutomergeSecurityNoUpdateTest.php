202e6ecfbac6bb607da3c7cda724b1993234ea07
Eirik S. Morland <eirik@morland.no>
2022-06-21 19:53:51 +0200

    Test updating the PR with no security update as well

--- test/integration/AutomergeSecurityNoUpdateTest.php
+++ test/integration/AutomergeSecurityNoUpdateTest.php
@@ -2,6 +2,9 @@
 
 namespace eiriksm\CosyComposerTest\integration;
 
+use Github\Exception\ValidationFailedException;
+use Violinist\Slug\Slug;
+
 /**
  * Test for automerge being enabled for security, but no security updates.
  */
@@ -16,8 +19,29 @@ class AutomergeSecurityNoUpdateTest extends ComposerUpdateIntegrationBase
     protected $hasAutoMerge = false;
     protected $checkPrUrl = true;
 
-    public function testAutomerge()
+    protected function createPullRequest(Slug $slug, array $params)
     {
+        if (!$this->isUpdate) {
+            return parent::createPullRequest($slug, $params);
+        }
+        throw new ValidationFailedException('I want you to update please');
+    }
+
+    /**
+     * @dataProvider getUpdateVariations
+     */
+    public function testAutomerge($should_have_updated)
+    {
+        $this->isUpdate = $should_have_updated;
+        $this->checkPrUrl = !$should_have_updated;
         $this->runtestExpectedOutput();
+    }
+
+    public function getUpdateVariations()
+    {
+        return [
+            [true],
+            [false],
+        ];
     }
 }

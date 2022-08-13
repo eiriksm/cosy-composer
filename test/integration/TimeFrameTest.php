587cb5fff30a40438bf6ee92d1f1508fdc97294e
Eirik Stanghelle Morland <eirik@morland.no>
2022-08-13 15:33:05 +0200

    Create TimeFrameTest.php

---
+++ test/integration/TimeFrameTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+/**
+ * Test for branch prefix with one_per option set.
+ */
+class TimeFrameTest extends ComposerUpdateIntegrationBase
+{
+    protected $packageForUpdateOutput = 'psr/log';
+    protected $packageVersionForFromUpdateOutput = '1.0.0';
+    protected $packageVersionForToUpdateOutput = '1.1.4';
+    protected $composerAssetFiles = 'composer.timeframe';
+
+    public function testTimeFrame()
+    {
+        $this->runtestExpectedOutput();
+    }
+}

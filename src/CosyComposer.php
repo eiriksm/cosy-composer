8cf3fc1675926e536823c356d59e75f944579fb0
Eirik Stanghelle Morland <eirik@morland.no>
2022-08-20 19:05:17 +0200

    Update CosyComposer.php

--- src/CosyComposer.php
+++ src/CosyComposer.php
@@ -1297,7 +1297,7 @@ class CosyComposer
     {
         if ($this->isPrivate) {
             $origin = 'origin';
-            if ($this->execCommand(["git", 'push', $origin, $branch_name '--force'])) {
+            if ($this->execCommand(["git", 'push', $origin, $branch_name, '--force'])) {
                 throw new GitPushException('Could not push to ' . $branch_name);
             }
         } else {

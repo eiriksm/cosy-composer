46321c079e25d9222cd616f017a08bbd47fa3087
Eirik Stanghelle Morland <eirik@morland.no>
2022-08-20 08:40:03 +0200

    Update CommandExecuter.php

--- src/CommandExecuter.php
+++ src/CommandExecuter.php
@@ -35,10 +35,10 @@ class CommandExecuter
         $this->processFactory = $factory;
     }
 
-    public function executeCommand($command, $log = true, $timeout = 120, array $env = [])
+    public function executeCommand(array $command, $log = true, $timeout = 120, array $env = [])
     {
         if ($log) {
-            $this->logger->log('info', new Message('Creating command ' . $command, Message::COMMAND));
+            $this->logger->log('info', new Message('Creating command ' . implode(' ', $command), Message::COMMAND));
         }
         $env = $this->getEnv() + $env + [
             'PATH' => __DIR__ . '/../../../../vendor/bin' . ':' . getenv('PATH'),

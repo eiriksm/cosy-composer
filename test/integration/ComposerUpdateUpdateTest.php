ea0b8174b95ff6360030c7fc99cf3ff284acdc4a
Eirik S. Morland <eirik@morland.no>
2024-01-23 15:28:24 +0100

    One more test

--- test/integration/ComposerUpdateUpdateTest.php
+++ test/integration/ComposerUpdateUpdateTest.php
@@ -2,8 +2,43 @@
 
 namespace eiriksm\CosyComposerTest\integration;
 
+use eiriksm\ArrayOutput\ArrayOutput;
+use eiriksm\CosyComposer\ProviderFactory;
+use eiriksm\CosyComposer\Providers\Github;
+use Violinist\Slug\Slug;
+
 class ComposerUpdateUpdateTest extends ComposerUpdateIntegrationBase
 {
+    public function testUpdateAvailableButUpdatedToOther()
+    {
+        $this->packageForUpdateOutput = 'drupal/core';
+        $this->packageVersionForFromUpdateOutput = '8.4.7';
+        $this->packageVersionForToUpdateOutput = '8.5.4';
+        $this->composerAssetFiles = 'composer-drupal-847';
+        $expected_pr = [
+            'base' => 'master',
+            'head' => 'drupalcore847848',
+            'title' => 'Update drupal/core from 8.4.7 to 8.4.8',
+            'body' => 'If you have a high test coverage index, and your tests for this pull request are passing, it should be both safe and recommended to merge this update.
+
+### Updated packages
+
+Some times an update also needs new or updated dependencies to be installed. Even if this branch is for updating one dependency, it might contain other installs or updates. All of the updates in this branch can be found here:
+
+- drupal/core: 8.4.8 (updated from 8.4.7)
+
+
+
+***
+This is an automated pull request from [Violinist](https://violinist.io/): Continuously and automatically monitor and update your composer dependencies. Have ideas on how to improve this message? All violinist messages are open-source, and [can be improved here](https://github.com/violinist-dev/violinist-messages).
+',
+            'assignees' => [],
+        ];
+        $this->checkPrUrl = true;
+        $this->setUp();
+        $this->runtestExpectedOutput();
+        self::assertEquals($expected_pr, $this->prParams);
+    }
 
     public function testUpdatedAndNewInstalled()
     {
@@ -14,7 +49,6 @@ class ComposerUpdateUpdateTest extends ComposerUpdateIntegrationBase
         $this->checkPrUrl = true;
         $this->setUp();
         $this->runtestExpectedOutput();
-        self::assertTrue(true);
         self::assertEquals([
             'base' => 'master',
             'head' => 'drupalcore880893',

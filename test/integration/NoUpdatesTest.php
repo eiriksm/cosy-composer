5a398553d799b2c2658b771457aa8f57ff820ea7
Eirik S. Morland <eirik@morland.no>
2024-01-26 15:22:53 +0100

    Increase coverage back to old

--- test/integration/NoUpdatesTest.php
+++ test/integration/NoUpdatesTest.php
@@ -69,4 +69,35 @@ class NoUpdatesTest extends Base
         $c->run();
         $this->assertEquals(true, $called);
     }
+
+    public function testNoUpdatesWorseDataLines()
+    {
+        $c = $this->getMockCosy();
+        $dir = '/tmp/' . uniqid();
+        mkdir($dir);
+        $c->setTmpDir($dir);
+        $this->updateJson = '{"installed": 1}';
+        $composer_contents = '{"require": {"drupal/core": "8.0.0"}}';
+        $composer_file = "$dir/composer.json";
+        file_put_contents($composer_file, $composer_contents);
+        $called = false;
+        $mock_executer = $this->createMock(CommandExecuter::class);
+        $mock_executer->method('executeCommand')
+            ->will($this->returnCallback(
+                function ($cmd) use (&$called) {
+                    $this->lastCommand = $cmd;
+                    $cmd_string = implode(' ', $cmd);
+                    if (strpos($cmd_string, 'rm -rf /tmp/') === 0) {
+                        $called = true;
+                    }
+                    return 0;
+                }
+            ));
+        $this->ensureMockExecuterProvidesLastOutput($mock_executer);
+        $c->setExecuter($mock_executer);
+        $this->assertEquals(false, $called);
+        $c->run();
+        $this->assertEquals(true, $called);
+        $this->assertOutputContainsMessage('Update data was in wrong format or missing. This is an error in violinist and should be reported', $c);
+    }
 }
--- test/integration/issues/Issue98Test.php
+++ test/integration/issues/Issue98Test.php
@@ -22,12 +22,16 @@ class Issue98Test extends ComposerUpdateIntegrationBase
     public function testIssue98()
     {
         self::assertEquals(false, $this->calledCorrectly);
+        $this->placeComposerLockContentsFromFixture('composer-lock-private.lock', $this->dir);
         $this->runtestExpectedOutput();
         $this->assertEquals($this->calledCorrectly, $this->calledCorrectly);
     }
 
     protected function handleExecutorReturnCallback($cmd, &$return)
     {
+        if ($cmd == $this->createExpectedCommandForPackage('eirik/private-pack')) {
+            $this->placeComposerLockContentsFromFixture('composer-lock-private.updated', $this->dir);
+        }
         if ($cmd === ["git", "clone", 'https://user-token:x-oauth-basic@github.com/eiriksm/private-pack.git', '/tmp/9f7527992e178cafad06d558b8f32ce8']) {
             $this->calledCorrectly = true;
         }

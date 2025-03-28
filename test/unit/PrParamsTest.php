b856d4c14ab425ad899ac7c2b26f718e6add0f26
Eirik S. Morland <eirik@morland.no>
2025-03-28 09:15:24 +0100

    Unify some pr params code, and add a unit test

---
+++ test/unit/PrParamsTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\unit;
+
+use eiriksm\CosyComposer\PrParamsCreator;
+use eiriksm\ViolinistMessages\ViolinistMessages;
+use eiriksm\ViolinistMessages\ViolinistUpdate;
+use PHPUnit\Framework\TestCase;
+use Violinist\Config\Config;
+use Violinist\Slug\Slug;
+
+class PrParamsTest extends TestCase
+{
+    /**
+     * Test the PR params.
+     *
+     * @dataProvider prParamsProvider
+     */
+    public function testPrParams(bool $is_private, Slug $slug, Config $config, $expected)
+    {
+        $msg_factory = new ViolinistMessages();
+        $pr_params_creator = new PrParamsCreator($msg_factory);
+        // Create some markdown that also includes that kind of summary detail
+        // kind of a thing.
+        $update = new ViolinistUpdate();
+        $update->setName('test/package');
+        $update->setCurrentVersion('1.0.0');
+        $update->setNewVersion('1.0.1');
+        $update->setPackageReleaseNotes([
+            '- [Release notes for tag 1.0.1](https://github.com/test/package/releases/tag/1.0.1)',
+        ]);
+        $body = $msg_factory->getPullRequestBody($update);
+        $fork_user = 'test';
+        $branch_name = 'derptest';
+        $title = $msg_factory->getPullRequestTitle($update);
+        $default_branch = 'develop';
+        $pr_params = $pr_params_creator->getPrParams($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        self::assertEquals($expected['title'], $pr_params['title']);
+        self::assertEquals($expected['base'], $pr_params['base']);
+        self::assertEquals($expected['head'], $pr_params['head']);
+        if ($slug->getProvider() === 'bitbucket.org') {
+            self::assertStringNotContainsString('<details>', $pr_params['body']);
+        }
+    }
+
+    public static function prParamsProvider()
+    {
+        return [
+            [
+                true,
+                new Slug(),
+                new Config(),
+                [
+                    'base' => 'develop',
+                    'head' => 'derptest',
+                    'title' => 'Update test/package from 1.0.0 to 1.0.1
+',
+                ],
+            ],
+            [
+                false,
+                new Slug(),
+                new Config(),
+                [
+                    'base' => 'develop',
+                    'head' => 'test:derptest',
+                    'title' => 'Update test/package from 1.0.0 to 1.0.1
+',
+                ]
+            ],
+            [
+                false,
+                Slug::createFromUrl('https://bitbucket.org/test/repo'),
+                new Config(),
+                [
+                    'base' => 'develop',
+                    'head' => 'test:derptest',
+                    'title' => 'Update test/package from 1.0.0 to 1.0.1
+',
+                ]
+            ]
+        ];
+    }
+}

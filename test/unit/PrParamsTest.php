b01d8d8fdb58f238dcc7e67887645e0bc7db9c22
Eirik S. Morland <eirik@morland.no>
2025-03-28 15:16:49 +0100

    Update tests

--- test/unit/PrParamsTest.php
+++ test/unit/PrParamsTest.php
@@ -6,6 +6,7 @@ use eiriksm\CosyComposer\PrParamsCreator;
 use eiriksm\ViolinistMessages\ViolinistMessages;
 use eiriksm\ViolinistMessages\ViolinistUpdate;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 use Violinist\Config\Config;
 use Violinist\Slug\Slug;
 
@@ -16,7 +17,7 @@ class PrParamsTest extends TestCase
      *
      * @dataProvider prParamsProvider
      */
-    public function testPrParams(bool $is_private, Slug $slug, Config $config, $expected) : void
+    public function testPrParams(bool $is_private, Slug $slug, Config $config, $expected, $method_name = 'getPrParams') : void
     {
         $msg_factory = new ViolinistMessages();
         $pr_params_creator = new PrParamsCreator($msg_factory);
@@ -34,12 +35,37 @@ class PrParamsTest extends TestCase
         $branch_name = 'derptest';
         $title = $msg_factory->getPullRequestTitle($update);
         $default_branch = 'develop';
-        $pr_params = $pr_params_creator->getPrParams($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        $pr_params_creator->setLogger($this->createMock(LoggerInterface::class));
+        $pr_params_creator->setAssigneesAllowed(true);
+        if ($method_name === 'getPrParamsForGroup') {
+            $pr_params = $pr_params_creator->getPrParamsForGroup($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        } else {
+            $pr_params = $pr_params_creator->getPrParams($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        }
+        if (empty($expected['assignees'])) {
+            $expected['assignees'] = [];
+        }
+        self::assertEquals($expected['assignees'], $pr_params['assignees']);
         self::assertEquals($expected['title'], $pr_params['title']);
         self::assertEquals($expected['base'], $pr_params['base']);
         self::assertEquals($expected['head'], $pr_params['head']);
         if ($slug->getProvider() === 'bitbucket.org') {
             self::assertStringNotContainsString('<details>', $pr_params['body']);
+        }
+        // Now do the same, but set assignees allowed to false.
+        $pr_params_creator->setAssigneesAllowed(false);
+        if ($method_name === 'getPrParamsForGroup') {
+            $pr_params = $pr_params_creator->getPrParamsForGroup($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        } else {
+            $pr_params = $pr_params_creator->getPrParams($fork_user, $is_private, $slug, $branch_name, $body, $title, $default_branch, $config);
+        }
+        self::assertEquals($expected['title'], $pr_params['title']);
+        self::assertEquals($expected['base'], $pr_params['base']);
+        self::assertEquals($expected['head'], $pr_params['head']);
+        if ($is_private) {
+            self::assertEquals([], $pr_params['assignees']);
+        } else {
+            self::assertEquals($expected['assignees'], $pr_params['assignees']);
         }
     }
 
@@ -52,7 +78,7 @@ class PrParamsTest extends TestCase
      */
     public function testPrParamsForGroup(bool $is_private, Slug $slug, Config $config, $expected) : void
     {
-        $this->testPrParams($is_private, $slug, $config, $expected);
+        $this->testPrParams($is_private, $slug, $config, $expected, 'getPrParamsForGroup');
     }
 
     public static function prParamsProvider()
@@ -89,6 +115,50 @@ class PrParamsTest extends TestCase
                     'head' => 'test:derptest',
                     'title' => 'Update test/package from 1.0.0 to 1.0.1
 ',
+                ],
+            ],
+            [
+                true,
+                Slug::createFromUrl('https://bitbucket.org/test/repo'),
+                Config::createFromComposerData((object) [
+                    'extra' => (object) [
+                        'violinist' => (object) [
+                            'assignees' => [
+                                'testuser',
+                            ],
+                        ],
+                    ],
+                ]),
+                [
+                    'base' => 'develop',
+                    'head' => 'derptest',
+                    'title' => 'Update test/package from 1.0.0 to 1.0.1
+',
+                    'assignees' => [
+                        'testuser',
+                    ],
+                ],
+            ],
+            [
+                false,
+                Slug::createFromUrl('https://bitbucket.org/test/repo'),
+                Config::createFromComposerData((object) [
+                    'extra' => (object) [
+                        'violinist' => (object) [
+                            'assignees' => [
+                                'testuser',
+                            ],
+                        ],
+                    ],
+                ]),
+                [
+                    'base' => 'develop',
+                    'head' => 'test:derptest',
+                    'title' => 'Update test/package from 1.0.0 to 1.0.1
+',
+                    'assignees' => [
+                        'testuser',
+                    ],
                 ],
             ],
         ];

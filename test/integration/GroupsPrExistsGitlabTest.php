a1ceb0cc09f5c4b1d8670f523f03a8768b37f4cf
Eirik S. Morland <eirik@morland.no>
2025-04-03 20:12:11 +0200

    Add the one with gitlab exception

---
+++ test/integration/GroupsPrExistsGitlabTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+use Github\Exception\ValidationFailedException;
+use Gitlab\Exception\RuntimeException;
+use Violinist\Slug\Slug;
+
+class GroupsPrExistsGitlabTest extends GroupsPrExistsTest
+{
+
+    protected function createPullRequest(Slug $slug, array $params)
+    {
+        throw new RuntimeException('The PR exists already yeah');
+    }
+}

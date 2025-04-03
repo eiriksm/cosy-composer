e8e2cbb0702df6cec35906c0c3007ebff03d3d8e
Eirik S. Morland <eirik@morland.no>
2025-04-03 08:45:05 +0200

    Add a why not test for groups

---
+++ test/integration/NotUpdatedGroupsForDrupalCoreAndContribTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+use Violinist\ComposerUpdater\Exception\NotUpdatedException;
+
+class NotUpdatedGroupsForDrupalCoreAndContribTest extends ComposerUpdateIntegrationBase
+{
+    protected $composerAssetFiles = 'composer-group-contrib-and-core';
+    protected $updateJson = '{
+    "installed": [
+        {
+            "name": "drupal/coffee",
+            "direct-dependency": true,
+            "homepage": "https://drupal.org/project/coffee",
+            "source": "https://git.drupalcode.org/project/coffee",
+            "version": "2.0.0",
+            "latest": "2.0.1",
+            "latest-status": "semver-safe-update",
+            "description": "Provides an Alfred like search box to navigate within your site.",
+            "abandoned": false
+        },
+        {
+            "name": "drupal/core-composer-scaffold",
+            "direct-dependency": true,
+            "homepage": "https://www.drupal.org/project/drupal",
+            "source": "https://github.com/drupal/core-composer-scaffold/tree/11.1.4",
+            "version": "11.1.4",
+            "latest": "11.1.5",
+            "latest-status": "semver-safe-update",
+            "description": "A flexible Composer project scaffold builder.",
+            "abandoned": false
+        },
+        {
+            "name": "drupal/core-project-message",
+            "direct-dependency": true,
+            "homepage": "https://www.drupal.org/project/drupal",
+            "source": "https://github.com/drupal/core-project-message/tree/11.1.4",
+            "version": "11.1.4",
+            "latest": "11.1.5",
+            "latest-status": "semver-safe-update",
+            "description": "Adds a message after Composer installation.",
+            "abandoned": false
+        },
+        {
+            "name": "drupal/core-recommended",
+            "direct-dependency": true,
+            "homepage": null,
+            "source": "https://github.com/drupal/core-recommended/tree/11.1.4",
+            "version": "11.1.4",
+            "latest": "11.1.5",
+            "latest-status": "semver-safe-update",
+            "description": "Core and its dependencies with known-compatible minor versions. Require this project INSTEAD OF drupal/core.",
+            "abandoned": false
+        },
+        {
+            "name": "drupal/gin",
+            "direct-dependency": true,
+            "homepage": "https://www.drupal.org/project/gin",
+            "source": "https://git.drupalcode.org/project/gin",
+            "version": "4.0.5",
+            "latest": "4.0.6",
+            "latest-status": "semver-safe-update",
+            "description": "For a better Admin and Content Editor Experience.",
+            "abandoned": false
+        }
+    ]
+}
+';
+
+    public function testWhyNotUpdatesGroupedContribAndCore()
+    {
+        $this->runtestExpectedOutput();
+        $output = $this->cosy->getOutput();
+        // All of these should be attempted a why not on.
+        $packages_and_versions = [
+            'drupal/coffee' => '2.0.1',
+            'drupal/core-composer-scaffold' => '11.1.5',
+            'drupal/core-project-message' => '11.1.5',
+            'drupal/core-recommended' => '11.1.5',
+            'drupal/gin' => '4.0.6',
+        ];
+        foreach ($packages_and_versions as $package => $version) {
+            $found = false;
+            foreach ($output as $message) {
+                $context = $message->getContext();
+                if (empty($context)) {
+                    continue;
+                }
+                if (empty($context['command'])) {
+                    continue;
+                }
+                if (strpos($context['command'], "composer why-not $package $version") === 0) {
+                    $found = true;
+                }
+            }
+            self::assertTrue($found, 'Did not find why not for ' . $package);
+        }
+    }
+
+    public function handleExecutorReturnCallback(array $cmd, &$return)
+    {
+        $this->stdout = '';
+        $this->errorOutput = '';
+        // If it contains all of these items it seems to be the contrib update
+        // command.
+        $command_parts = ['composer', 'update', 'drupal/coffee', 'drupal/gin'];
+        if (count(array_intersect($command_parts, $cmd)) === count($command_parts)) {
+            throw new NotUpdatedException();
+        }
+        // If its the core thing, let's do that.
+        $command_parts_for_core = [
+            'composer',
+            'update',
+            'drupal/core-project-message',
+            'drupal/core-recommended',
+        ];
+        if (count(array_intersect($command_parts_for_core, $cmd)) === count($command_parts_for_core)) {
+            throw new NotUpdatedException();
+        }
+    }
+}

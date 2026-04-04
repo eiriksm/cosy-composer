faf62f7dbcffe0aa171e06281e994539c23ac9a1
Eirik Stanghelle Morland <eirik@morland.no>
2026-04-04 07:20:14 +0200

    Fix PHPStan type hints in CosyComposerChangelogTest and shared traits (#466)

--- test/GetCosyTrait.php
+++ test/GetCosyTrait.php
@@ -14,7 +14,7 @@ use Violinist\SymfonyCloudSecurityChecker\SecurityChecker;
 
 trait GetCosyTrait
 {
-    protected function getMockCosy($dir = null) : CosyComposer
+    protected function getMockCosy(?string $dir = null) : CosyComposer
     {
         $executer = $this->createMock(CommandExecuter::class);
         $c = new CosyComposer($executer);

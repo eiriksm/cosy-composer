ad4dc2bc95897cb0460a55afdb026397194aa4cc
Eirik S. Morland <eirik@morland.no>
2026-04-07 22:04:56 +0200

    Test coverage

---
+++ test/unit/Providers/BitbucketProviderTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\unit\Providers;
+
+use Bitbucket\Client;
+use eiriksm\CosyComposer\Providers\Bitbucket;
+use PHPUnit\Framework\TestCase;
+use Violinist\Slug\Slug;
+
+class BitbucketProviderTest extends TestCase
+{
+    private function createBitbucketWithBranches(array $branches): Bitbucket
+    {
+        $mock = $this->getMockBuilder(Bitbucket::class)
+            ->setConstructorArgs([$this->createMock(Client::class)])
+            ->onlyMethods(['getBranches'])
+            ->getMock();
+        $mock->method('getBranches')
+            ->willReturn($branches);
+        return $mock;
+    }
+
+    public function testDefaultBaseTimestamp(): void
+    {
+        $slug = Slug::createFromUrl('http://bitbucket.org/testUser/testRepo');
+        $provider = $this->createBitbucketWithBranches([
+            [
+                'name' => 'main',
+                'target' => [
+                    'hash' => 'abcd1234',
+                    'date' => '2025-01-15T10:30:00+00:00',
+                ],
+            ],
+        ]);
+        $this->assertEquals('2025-01-15T10:30:00+00:00', $provider->getDefaultBaseTimestamp($slug, 'main'));
+    }
+
+    public function testDefaultBaseTimestampReturnsNullForMissingBranch(): void
+    {
+        $slug = Slug::createFromUrl('http://bitbucket.org/testUser/testRepo');
+        $provider = $this->createBitbucketWithBranches([
+            [
+                'name' => 'other',
+                'target' => [
+                    'hash' => 'abcd1234',
+                    'date' => '2025-01-15T10:30:00+00:00',
+                ],
+            ],
+        ]);
+        $this->assertNull($provider->getDefaultBaseTimestamp($slug, 'main'));
+    }
+
+    public function testDefaultBaseTimestampReturnsNullForMissingDate(): void
+    {
+        $slug = Slug::createFromUrl('http://bitbucket.org/testUser/testRepo');
+        $provider = $this->createBitbucketWithBranches([
+            [
+                'name' => 'main',
+                'target' => [
+                    'hash' => 'abcd1234',
+                ],
+            ],
+        ]);
+        $this->assertNull($provider->getDefaultBaseTimestamp($slug, 'main'));
+    }
+}
